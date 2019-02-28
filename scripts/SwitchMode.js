0536ec8f0d12b06c2c45018fb75355beeacadfa8
Andy <andy.mcewan@artos.io>
2019-02-28 11:56:04 +0000

    Coverage prep and tidy up

---
+++ scripts/SwitchMode.js
@@ -0,0 +1,56 @@
+const fs = require('fs');
+const path = require('path');
+
+function traverseDir(_dir, _files) {
+  fs.readdirSync(_dir).forEach(file => {
+    let fullPath = path.join(_dir, file);
+    if (fs.lstatSync(fullPath).isDirectory()) {
+      traverseDir(fullPath, _files);
+    } else {
+      _files.push(fullPath);
+    }
+  });
+}
+
+function swap(_from, _to, _contract) {
+  let contents = fs.readFileSync(_contract, 'utf8');
+  for (var i = 0; i < _from.length; i++) {
+    contents = contents.split(_from[i]).join(_to[i]);
+  }
+  fs.writeFileSync(_contract, contents, 'utf8');
+}
+
+function main() {
+  const mode = process.argv.slice(2)[0].trim();
+  console.log('** Switching code to', mode, 'mode');
+  let contracts = [];
+
+  const debug = [
+    'assert(',
+    '/* RELEASE_ONLY:',
+    ':RELEASE_ONLY */',
+    '// RELEASE_ONLY ',
+    '// DEBUG_ONLY:',
+    '// :DEBUG_ONLY',
+    '/* DEBUG_ONLY */ '
+  ];
+
+  const release = [
+    '// DEBUG_ONLY assert(',
+    '// RELEASE_ONLY:',
+    '// :RELEASE_ONLY',
+    '/* RELEASE_ONLY */ ',
+    '/* DEBUG_ONLY:',
+    ':DEBUG_ONLY */',
+    '// DEBUG_ONLY '
+  ];
+
+  traverseDir(path.join(__dirname, '../contracts'), contracts);
+
+  contracts.forEach(contract => {
+    if (mode === 'debug') swap(release, debug, contract);
+    else if (mode === 'release') swap(debug, release, contract);
+  });
+}
+
+main();
